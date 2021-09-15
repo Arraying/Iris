dd5ac3339ad29c8da076d7644d45887282db26cd
Arraying <paul.huebner@googlemail.com>
2021-09-15 16:36:57 +0200

    Create setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Iris',
+    version='1.0.0',
+    packages=['scikit-learn'],
+    url='',
+    license='LICENSE.txt',
+    author='Arraying',
+    author_email='',
+    description='Basic Bayesian Naïve classification of Iris flowers'
+)
